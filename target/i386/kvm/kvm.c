9c004b4e6526ab202fd638f2d552e49e5b48be32
Wei Wang <wei.w.wang@intel.com>
2023-03-17 16:43:06 +0800

    i386/kvm: allow migration to put registers of a TD guest
    Some emulated register states need to be migrated, so return in advance
    from kvm_arch_put_registers only for debuggable TD.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -5001,7 +5001,7 @@ int kvm_arch_put_registers(CPUState *cpu, int level)
     assert(cpu_is_stopped(cpu) || qemu_cpu_is_self(cpu));
 
     /* TODO: Allow accessing guest state for debug TDs. */
-    if (is_tdx_vm()) {
+    if (tdx_debug_enabled()) {
         CPUX86State *env = &x86_cpu->env;
 
         /*
@@ -5009,8 +5009,7 @@ int kvm_arch_put_registers(CPUState *cpu, int level)
          * Now KVM has workaround to emulate
          * #BP injection to support GDB stub feature.
          */
-        if (tdx_debug_enabled() &&
-            (env->exception_pending == 1) &&
+        if ((env->exception_pending == 1) &&
             (env->exception_nr == 3))
             return kvm_put_vcpu_events(x86_cpu, level);
 
