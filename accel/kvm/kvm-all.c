intel-staging/qemu-tdx

KVM: invoke the memory region listener when memory attribute converted
Once the hypervisor exits to userspace to convert the page from private
to shared ro vice versa, notify the state change via memory listener so
that other components (e.g. VFIO) can be notified.

Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

accel/kvm/kvm-all.c
@@ -2976,6 +2976,7 @@ int kvm_convert_memory(hwaddr start, hwaddr size, bool shared_to_private)
          * operation on underlying file descriptor is only for releasing
          * unnecessary pages.
          */
+        memory_region_convert_mem_attr(&section, !shared_to_private);
         (void)ram_block_convert_range(rb, offset, size, shared_to_private);
     } else {
         MemoryRegion *mr = section.mr;

intel/qemu-td-partitioning

accel/kvm/kvm-all.c
@@ -3099,6 +3099,7 @@ int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
         addr = memory_region_get_ram_ptr(section.mr) +
                section.offset_within_region;
         rb = qemu_ram_block_from_host(addr, false, &offset);
+        memory_region_convert_mem_attr(&section, !to_private);
         /*
          * With KVM_SET_MEMORY_ATTRIBUTES by kvm_set_memory_attributes(),
          * operation on underlying file descriptor is only for releasing