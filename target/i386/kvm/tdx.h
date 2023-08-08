a28b52edb3adc7191bd6ba82adb5dcc08729144e
Wei Wang <wei.w.wang@intel.com>
2023-08-08 17:28:38 +0800

    i386/tdx: initiate pre-migration
    Pre-migration is initiated by user providing the vsock port via the
    qom command, e.g. "qom-set /objects/tdx0/ vsockport 1234". Upon
    receiving the command, QEMU sends the KVM_TDX_SET_MIGRATION_INFO command,
    along with the migration info, including the vsock port and being source
    or destination TD, to KVM, and KVM will notify MigTD to get the info
    data and start the pre-migration setup.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- target/i386/kvm/tdx.h
+++ target/i386/kvm/tdx.h
@@ -149,6 +149,7 @@ typedef struct TdxGuest {
 
     TdxVmcallService vmcall_service;
 
+    uint32_t vsockport;
     uint32_t migtd_pid;
     uint64_t migtd_attr;
     uint8_t migtd_hash[48];  /* sha348 digest */
