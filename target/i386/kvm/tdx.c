intel/qemu-td-partitioning

commit de6de7455b02aa1c45ef7d6f6b1e44dcad3b5182

i386/tdx: Limit the range size for MapGPA
If the range for TDG.VP.VMCALL<MapGPA> is too large, process the limited
size and return retry error.  It's bad for VMM to take too long time,
e.g. second order, with blocking vcpu execution.  It results in too many
missing timer interrupts.

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

target/i386/kvm/tdx.c
@@ -1035,6 +1035,9 @@ static hwaddr tdx_shared_bit(X86CPU *cpu)
     return (cpu->phys_bits > 48) ? BIT_ULL(51) : BIT_ULL(47);
 }
 
+/* 64MB at most in one call. What value is appropriate? */
+#define TDX_MAP_GPA_MAX_LEN     (64 * 1024 * 1024)
+
 static void tdx_handle_map_gpa(X86CPU *cpu, struct kvm_tdx_vmcall *vmcall)
 {
     hwaddr addr_mask = (1ULL << cpu->phys_bits) - 1;
@@ -1042,6 +1045,7 @@ static void tdx_handle_map_gpa(X86CPU *cpu, struct kvm_tdx_vmcall *vmcall)
     hwaddr gpa = vmcall->in_r12 & ~shared_bit;
     bool private = !(vmcall->in_r12 & shared_bit);
     hwaddr size = vmcall->in_r13;
+    bool retry = false;
     int ret = 0;
 
     vmcall->status_code = TDG_VP_VMCALL_INVALID_OPERAND;
@@ -1063,12 +1067,22 @@ static void tdx_handle_map_gpa(X86CPU *cpu, struct kvm_tdx_vmcall *vmcall)
         return;
     }
 
+    if (size > TDX_MAP_GPA_MAX_LEN) {
+        retry = true;
+        size = TDX_MAP_GPA_MAX_LEN;
+    }
+
     if (size > 0) {
         ret = kvm_convert_memory(gpa, size, private);
     }
 
     if (!ret) {
-        vmcall->status_code = TDG_VP_VMCALL_SUCCESS;
+        if (retry) {
+            vmcall->status_code = TDG_VP_VMCALL_RETRY;
+            vmcall->out_r11 = gpa + size;
+        } else {
+            vmcall->status_code = TDG_VP_VMCALL_SUCCESS;
+        }
     }
 }
 

commit 5f01df30e996a7ac437ed2d4f749adb38541517e

i386/tdx: Fix tdx_handle_map_gpa() to include shared bit
tdx_handle_map_gpa() should return the continuing GPA on TDG.VP_VMCALL_SUCCESS.
Shared bit was missing.  Add the bit when the GPA is shared.

Fixes: de6de7455b02a ("i386/tdx: Limit the range size for MapGPA")

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

target/i386/kvm/tdx.c
@@ -1683,6 +1683,9 @@ static void tdx_handle_map_gpa(X86CPU *cpu, struct kvm_tdx_vmcall *vmcall)
         if (retry) {
             vmcall->status_code = TDG_VP_VMCALL_RETRY;
             vmcall->out_r11 = gpa + size;
+            if (!private) {
+                vmcall->out_r11 |= shared_bit;
+            }
         } else {
             vmcall->status_code = TDG_VP_VMCALL_SUCCESS;
         }