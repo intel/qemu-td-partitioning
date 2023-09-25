intel/qemu-td-partitioning

commit 4b1755209e1d4fcd6eff19fb67342b654eaafb8c

kvm: refactor kvm_convert_memory
Put the private mr and non-private mr related memory convert into
kvm_convert_memory_private_mr and kvm_convert_memory_shared_mr to make
kvm_convert_memory simpler and more readable.

The subfunctions, e.g. kvm_convert_memory_private_mr will be able to be
invoked specifically for a private memory region, e.g. kvm_set_phys_mem
is updated to call this function to make the memory private after the
memslot is updated to KVM. This will go through the necessary part of
the memory convert path (i.e. set the KVM memory attr to private and
update the cgs_bmap, the discard path isn't needed as this is called
when launching the guest and the shared memory fd doesn't have an
allocation of memory yet).

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

accel/kvm/kvm-all.c
@@ -1511,7 +1511,8 @@ static void kvm_set_phys_mem(KVMMemoryListener *kml,
         }
 
         if (memory_region_is_default_private(mr)) {
-            err = kvm_set_memory_attributes_private(start_addr, slot_size);
+            err = kvm_convert_memory_private_mr(start_addr,
+                                                slot_size, true, false);
             if (err) {
                 error_report("%s: failed to set memory attribute private: %s\n",
                              __func__, strerror(-err));
@@ -3074,13 +3075,65 @@ static void kvm_eat_signals(CPUState *cpu)
     } while (sigismember(&chkset, SIG_IPI));
 }
 
+int kvm_convert_memory_private_mr(hwaddr start, hwaddr size,
+                                  bool to_private, bool need_discard)
+{
+    int ret;
+    ram_addr_t offset;
+    RAMBlock *rb = qemu_ram_block_from_hwaddr(start, size, &offset);
+    if (!rb) {
+        return -1;
+    }
+
+    if (to_private) {
+        ret = kvm_set_memory_attributes_private(start, size);
+    } else {
+        ret = kvm_set_memory_attributes_shared(start, size);
+    }
+
+    if (ret) {
+        return ret;
+    }
+
+    if (need_discard) {
+        /*
+         * With KVM_SET_MEMORY_ATTRIBUTES by kvm_set_memory_attributes(),
+         * operation on underlying file descriptor is only for releasing
+         * unnecessary pages.
+         */
+        ram_block_convert_range(rb, offset, size, to_private);
+    }
+
+    ram_block_update_cgs_bmap(rb, offset, size, to_private);
+    return 0;
+}
+
+static int kvm_convert_memory_shared_mr(MemoryRegion *mr, hwaddr start,
+                                        hwaddr size, bool to_private)
+{
+    /*
+     * Because vMMIO region must be shared, guest TD may convert vMMIO
+     * region to shared explicitly.  Don't complain such case.  See
+     * memory_region_type() for checking if the region is MMIO region.
+     */
+    if (to_private ||
+        memory_region_is_ram(mr) ||
+        memory_region_is_ram_device(mr) ||
+        memory_region_is_rom(mr) ||
+        memory_region_is_romd(mr)) {
+        warn_report("Convert non guest-memfd backed memory region"
+                    "(0x%"HWADDR_PRIx" ,+ 0x%"HWADDR_PRIx") of %s to %s",
+                    start, size, mr->name, to_private ? "private" : "shared");
+        return -1;
+    }
+
+    return 0;
+}
+
 int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
 {
     MemoryRegionSection section;
-    void *addr;
-    RAMBlock *rb;
-    ram_addr_t offset;
-    int ret = -1;
+    int ret = 0;
 
     trace_kvm_convert_memory(start, size, to_private ? "shared_to_private" : "private_to_shared");
     section = memory_region_find(get_system_memory(), start, size);
@@ -3094,59 +3147,24 @@ int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
          * OVMF assigns 32bit PCI MMIO region to
          * [top of low memory: typically 2GB=0xC000000,  0xFC00000)
          */
-        if (!to_private) {
-            ret = 0;
-        }
-        return ret;
-    }
-
-    if (memory_region_can_be_private(section.mr) || memory_region_is_private_mmio(section.mr)) {
         if (to_private) {
-            ret = kvm_set_memory_attributes_private(start, size);
-        } else {
-            ret = kvm_set_memory_attributes_shared(start, size);
+            ret = -1;
         }
-
-        if (ret) {
-            return ret;
-        }
-
-        addr = memory_region_get_ram_ptr(section.mr) +
-               section.offset_within_region;
-        rb = qemu_ram_block_from_host(addr, false, &offset);
-        memory_region_convert_mem_attr(&section, !to_private);
-
-        if (memory_region_is_private_mmio(section.mr)) {
-            ret = 0;
-        } else {
-            /*
-            * With KVM_SET_MEMORY_ATTRIBUTES by kvm_set_memory_attributes(),
-            * operation on underlying file descriptor is only for releasing
-            * unnecessary pages.
-            */
-            ram_block_convert_range(rb, offset, size, to_private);
-        }
-    } else {
-        MemoryRegion *mr = section.mr;
-
-        /*
-         * Because vMMIO region must be shared, guest TD may convert vMMIO
-         * region to shared explicitly.  Don't complain such case.  See
-         * memory_region_type() for checking if the region is MMIO region.
-         */
-        if (to_private ||
-            memory_region_is_ram(mr) ||
-            memory_region_is_ram_device(mr) ||
-            memory_region_is_rom(mr) ||
-            memory_region_is_romd(mr)) {
-            warn_report("Convert non guest-memfd backed memory region (0x%"HWADDR_PRIx" ,+ 0x%"HWADDR_PRIx") of %s to %s",
-                        start, size, mr->name, to_private ? "private" : "shared");
-	    } else {
-		    ret = 0;
-	    }
-
+        goto out;
     }
 
+    if (memory_region_can_be_private(section.mr) ||
+        memory_region_is_private_mmio(section.mr)) {
+        bool need_discard = !memory_region_is_private_mmio(section.mr);
+
+        ret = kvm_convert_memory_private_mr(start, size, to_private,
+                                            need_discard);
+    } else {
+        ret = kvm_convert_memory_shared_mr(section.mr,
+                                           start, size, to_private);
+    }
+
+out:
     memory_region_unref(section.mr);
     return ret;
 }

include/exec/cpu-common.h
@@ -78,6 +78,8 @@ ram_addr_t qemu_ram_addr_from_host_nofail(void *ptr);
 RAMBlock *qemu_ram_block_by_name(const char *name);
 RAMBlock *qemu_ram_block_from_host(void *ptr, bool round_offset,
                                    ram_addr_t *offset);
+RAMBlock *qemu_ram_block_from_hwaddr(hwaddr start, hwaddr size,
+                                     ram_addr_t *offset);
 ram_addr_t qemu_ram_block_host_offset(RAMBlock *rb, void *host);
 void qemu_ram_set_idstr(RAMBlock *block, const char *name, DeviceState *dev);
 void qemu_ram_unset_idstr(RAMBlock *block);
@@ -174,6 +176,8 @@ int qemu_ram_foreach_block(RAMBlockIterFunc func, void *opaque);
 int ram_block_discard_range(RAMBlock *rb, uint64_t start, size_t length);
 int ram_block_convert_range(RAMBlock *rb, uint64_t start, size_t length,
                             bool shared_to_private);
+void ram_block_update_cgs_bmap(RAMBlock *rb, uint64_t start,
+                               size_t length, bool shared_to_private);
 
 #endif
 

include/sysemu/kvm.h
@@ -588,6 +588,8 @@ int kvm_set_memory_attributes_private(hwaddr start, hwaddr size);
 int kvm_set_memory_attributes_shared(hwaddr start, hwaddr size);
 
 int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private);
+int kvm_convert_memory_private_mr(hwaddr start, hwaddr size,
+                                  bool to_private, bool need_discard);
 
 typedef void (*set_memory_region_debug_ops)(void *handle, MemoryRegion *mr);
 void kvm_setup_set_memory_region_debug_ops(struct KVMState *s,

softmmu/physmem.c
@@ -2257,6 +2257,26 @@ found:
     return block;
 }
 
+RAMBlock *qemu_ram_block_from_hwaddr(hwaddr start, hwaddr size,
+                                     ram_addr_t *offset)
+{
+    MemoryRegionSection section;
+    RAMBlock *rb;
+    void *addr;
+
+    section = memory_region_find(get_system_memory(), start, size);
+    if (!section.mr) {
+        return NULL;
+    }
+    addr = memory_region_get_ram_ptr(section.mr) +
+           section.offset_within_region;
+    rb = qemu_ram_block_from_host(addr, false, offset);
+
+    memory_region_unref(section.mr);
+
+    return rb;
+}
+
 /*
  * Finds the named RAMBlock
  *
@@ -3876,7 +3896,7 @@ bool ram_block_discard_is_required(void)
 int ram_block_convert_range(RAMBlock *rb, uint64_t start, size_t length,
                             bool shared_to_private)
 {
-    int fd, ret;
+    int fd;
 
     if (!rb || rb->gmem_fd < 0) {
         return -1;
@@ -3906,17 +3926,22 @@ int ram_block_convert_range(RAMBlock *rb, uint64_t start, size_t length,
         fd = rb->gmem_fd;
     }
 
-    ret = ram_block_discard_range_fd(rb, start, length, fd);
-    if (!ret) {
-        uint64_t bit_start = start / rb->page_size;
-        uint64_t bit_length = length / rb->page_size;
+    return ram_block_discard_range_fd(rb, start, length, fd);
+}
 
-        if (shared_to_private) {
-            bitmap_set(rb->cgs_bmap, bit_start, bit_length);
-        } else {
-            bitmap_clear(rb->cgs_bmap, bit_start, bit_length);
-        }
+void ram_block_update_cgs_bmap(RAMBlock *rb, uint64_t start,
+                               size_t length, bool shared_to_private)
+{
+    uint64_t bit_start = start >> TARGET_PAGE_BITS;
+    uint64_t bit_length = length >> TARGET_PAGE_BITS;
+
+    if (!rb->cgs_bmap) {
+        return;
     }
 
-    return ret;
+    if (shared_to_private) {
+        bitmap_set(rb->cgs_bmap, bit_start, bit_length);
+    } else {
+        bitmap_clear(rb->cgs_bmap, bit_start, bit_length);
+    }
 }

commit 0d89228200a3ce50b8e387a705d36c34d98b4694

KVM: add back the missing memory attr convert callback
Fixes: 4b1755209e ("kvm: refactor kvm_convert_memory")
Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

accel/kvm/kvm-all.c
@@ -3184,6 +3184,7 @@ int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private,
 
         ret = kvm_convert_memory_private_mr(start, size, to_private,
                                             need_discard, cpu_index);
+        memory_region_convert_mem_attr(&section, !to_private);
     } else {
         ret = kvm_convert_memory_shared_mr(section.mr,
                                            start, size, to_private);